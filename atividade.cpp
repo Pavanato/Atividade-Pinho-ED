69bd2650474488adb1076a0129fc786c5a2dba79
Pavanato <tandera837@hotmail.com>
2024-03-25 08:09:01 -0300

    funcao abs

--- atividade.cpp
+++ atividade.cpp
@@ -5,7 +5,43 @@ using std::cout;
 using std::endl;
 using std::string;
 
+
+// Protótipos das funções
+
+void valorAbs(int &irefNumero);
+
 int main() 
 {
-  return 0
+  cout << "Teste de valor absoluto: " << endl;
+
+  int iNumero = -10;
+
+  cout << "Número: " << iNumero << endl;
+
+  valorAbs(iNumero);
+
+  cout << "Número: " << iNumero << endl;
+
+  int iNumero2 = 20;
+
+  cout << "Número: " << iNumero2 << endl;
+
+  valorAbs(iNumero2);
+
+  cout << "Número: " << iNumero2 << endl;
+
+  cout << "=======================================" << endl;
+
+
+  return 0;
+}
+
+// Utilizando protótipos, notação Húngara e referências:
+
+//Reescrever uma função matemática, no caso o abs
+
+void valorAbs(int &irefNumero) {
+  if (irefNumero < 0) {
+    irefNumero = -irefNumero;
+  }
 }
